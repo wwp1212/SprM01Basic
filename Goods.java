b90c4206d1d709433a61150f08c95c22e86cfc9e
wwp1212 <45932028+wwp1212@users.noreply.github.com>
2019-01-17 09:49:35 +0800

    Add files via upload
    第一次上传

---
+++ Goods.java
@@ -0,0 +1,43 @@
+package cn.sxt01.entity;
+
+public class Goods {
+	private int id;
+	private String name;
+	private int price;
+	private int catid;
+	private String remarks;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getPrice() {
+		return price;
+	}
+	public void setPrice(int price) {
+		this.price = price;
+	}
+	public int getCatid() {
+		return catid;
+	}
+	public void setCatid(int catid) {
+		this.catid = catid;
+	}
+	public String getRemarks() {
+		return remarks;
+	}
+	public void setRemarks(String remarks) {
+		this.remarks = remarks;
+	}
+	
+	
+	
+
+}
